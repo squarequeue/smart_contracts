5b4ad018c82737acc6e9eae6ffffbbbc42db8f09
squarequeue <squarequeue@gmail.com>
2020-04-15 01:11:53 -0700

    replace ver2. allocation

--- IEO/Square_Queue_IEO_contract.sol
+++ IEO/Square_Queue_IEO_contract.sol
@@ -6,8 +6,8 @@ pragma solidity 0.5.14;
  * @author SquareQueue - SquareQueue@gmail.com https://github.com/squarequeue
  *
  * Unsold SQ coins at IEO will be burned out. There is no SQ supply other than IEO and StakeHoler(like POS) rewards.
- * SQ team will be allocated 5% SQ coins after 2years. Befor That time, it is 0%
- * Max supply is 1 billion and IEO supply is 450 million.
+ * SQ team will be allocated 1% SQ coins after 2years. Befor That time, it is 0%
+ * Max supply is 50,000,000‬ and IEO supply is 10,000,000‬
  */
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Burnable.sol";
@@ -15,7 +15,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 
 contract SQ_IEO_token is ERC20, ERC20Detailed, ERC20Burnable {
     uint8 public constant DECIMALS = 18;
-    uint256 public INITIAL_IEO_SUPPLY = 450000000 * (10 ** uint256(DECIMALS)); 
+    uint256 public INITIAL_IEO_SUPPLY = 10000000 * (10 ** uint256(DECIMALS)); 
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
